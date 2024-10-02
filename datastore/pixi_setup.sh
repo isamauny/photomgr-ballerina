c30947c402d288630b17e788d973a502677e87dd
isamauny <isabelle@akapelasolutions.com>
2024-10-02 18:05:45 +0200

    Docker build dependencies

---
+++ datastore/pixi_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+# Wait for MongoDB to start
+sleep 5
+
+# Restore the Pixi database
+mongorestore --username $MONGO_INITDB_ROOT_USERNAME --password $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin --db=Pixidb /tmp/PixiDbDump/Pixidb
+
+# Create a read-write user for the Pixi database
+mongo admin -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --authenticationDatabase admin --eval "db.getSiblingDB('Pixidb').createUser({user: 'pixiuser', pwd: 'devuserXXX', roles: [{role: 'readWrite', db: 'Pixidb'}]})"
